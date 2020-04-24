a514c600fbc11ca32185a2c14b2dcf7c4d95c162
Dhairya Jariwala <dhairyajariwala26799@gmail.com>
2020-04-24 16:04:03 +0530

    Added Jquery in localstorage

--- templates/inc/header.php
+++ templates/inc/header.php
@@ -6,11 +6,10 @@
         </title>
 
         <link rel="stylesheet" href="css/materialize.min.css" type="text/css">
-        
+
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     		<!-- Favicon -->
     		<link href="img/favicon.png" type="image/png" rel="icon">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script src="javascript/jquery.min.js"></script>
         <script src="javascript/materialize.min.js"></script>
